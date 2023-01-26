d2874ee95dd7621db5b1b4a451f60662a3820fd4
milliorn <scottmilliorn@gmail.com>
2023-01-25 17:50:13 -0800

    post route, server listen

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const webpush = require("web-push");
 
 // init express
 const app = express();
+const port = process.env.PORT || 5000;
+app.use(bodyParser.json());
 
 // VAPID keys should be generated only once. This gets our keys from .env. They are used to identify who is sending the push notifications
 const publicVapidKey = process.env.publicVapidKey;
@@ -17,3 +19,24 @@ const email = process.env.email;
 
 // When making requests where you want to define VAPID details, call this method before sendNotification() or pass in the details and options to sendNotification.
 webpush.setVapidDetails(email, publicVapidKey, privateVapidKey);
+
+// subscribe to routes
+app.post("/subscribe", (req, res) => {
+  // Get push subscription object
+  const subscription = req.body;
+
+  // Send 201 - created resource
+  res.status(201).json({});
+
+  // Create the payload
+  const payload = JSON.stringify({ title: "Push Notification Test" });
+
+  // Pass object into send notification
+  webpush
+    .sendNotification(subscription, payload)
+    .catch((err) => console.log(err));
+});
+
+app.listen(port, () => {
+  console.log(`Server at http://localhost:${port}`);
+});
